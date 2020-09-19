020a042a5a83d865dfdc764d794e75b4a9ae02e2
Jeison Alarcon <alarcon_osorio@hotmail.com>
2020-09-18 20:36:26 -0500

    Update parameters.php

--- e-commerce/shopping/app/config/parameters.php
+++ e-commerce/shopping/app/config/parameters.php
@@ -5,7 +5,7 @@
     'database_port' => '',
     'database_name' => 'u644237725_cosmeticos',
     'database_user' => 'u644237725_cosmeticos',
-    'database_password' => 'Jeison1010192548**',
+    'database_password' => 'password',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
